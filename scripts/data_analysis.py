9c2df39d634abd7e48ece451e66c3b743d0dce80
Becca <hihibecca@adpost.com.tw>
2024-06-26 18:22:25 +0800

    0826_still ongoing

--- scripts/data_analysis.py
+++ scripts/data_analysis.py
@@ -3,15 +3,17 @@ from datetime import datetime
 import database
 db = database.SQLiteOperation()
 
-today = datetime.today().strftime('%Y-%m-%d')
+today = datetime.today().strftime('%Y-%m-%d') # 2024-06-24 for test
 board = 'basketballTW'
 query = f'''
 SELECT * FROM Article
-WHERE date = {today}
+WHERE date = '{today}' 
 AND article_id IN (
     SELECT article_id FROM Overview
-    WHERE board = {board}
+    WHERE board = '{board}'
+    AND popularity = 100
 )
-'''
+''' 
+
 data = db.select_query(query)
 # %%
