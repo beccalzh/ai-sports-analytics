d9677ce0da388ccde090b98e64aca846219f38ea
beccalzh <beccalin.8359@gmail.com>
2024-07-08 17:41:55 +0800

    0708_modify log structure & levels

--- scripts/log_decorator.py
+++ scripts/log_decorator.py
@@ -10,12 +10,11 @@ if not os.path.exists(log_dir):
 
 # create file name
 today = datetime.today().strftime('%m%d')
-script_name = os.path.splitext(os.path.basename(__file__))[0]
-log_filename = os.path.join(log_dir, f'{script_name}_{today}.log')
+log_filename = os.path.join(log_dir, f'{today}.log')
 
 # set logging
-logging.basicConfig(filename=log_filename, level=logging.INFO, 
-                    format='%(asctime)s:%(levelname)s:%(funcName)s:%(message)s')
+logging.basicConfig(filename=log_filename, level=logging.ERROR, 
+                    format='[%(asctime)s] %(levelname)s: %(message)s')
 
 def log_decorator(func):
     """decorator for logging function calls and errors"""
@@ -23,7 +22,6 @@ def log_decorator(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         try:
-            logging.info(f"Called {func.__name__} with args={args} kwargs={kwargs}")
             result = func(*args, **kwargs)
             return result
         except Exception as e:
