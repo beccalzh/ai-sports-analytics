20d3cf25e63a041b66432a42b2c1ff101af5a539
beccalzh <beccalin.8359@gmail.com>
2024-09-18 19:27:40 +0800

    0920

--- scripts/main.py
+++ scripts/main.py
@@ -2,10 +2,14 @@ import collect_articles
 import data_analysis
 import json
 from datetime import date
+import os
 
 def main():
-    # collect_articles.main()
+    collect_articles.main()
     da_out = data_analysis.main()
     today = date.today().strftime('%Y-%m-%d')
-    with open(f'/Users/becca/ai-sports-analytics/database/da-test/{today}.json', 'w') as f:
+    if '../database/da-test/' not in os.listdir():
+        os.mkdir('../database/da-test')
+    with open(f'../database/da-test/{today}.json', 'w') as f:
         json.dump(da_out, f)
+        print(da_out)
